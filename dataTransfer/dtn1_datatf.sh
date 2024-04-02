e9e7a46ee31ba7ea816dee836122609364ec1975
Emily Mutter <88211836+ebmutter42@users.noreply.github.com>
2024-04-02 09:32:55 -0500

    Update dtn1_datatf.sh

--- dataTransfer/dtn1_datatf.sh
+++ dataTransfer/dtn1_datatf.sh
@@ -1,5 +1,6 @@
 #this is part one of running the data transfer measurements on DTN1. 
 #due to the security measures of the DMZ disallowing students to be root, this must be set up in cron by Dr. Renfro
+#this is run in cron every 4 hours on the hour
 YYYYMMDDHHMM=$(date +%Y%m%d-%H%M)
 timeout --kill-after=1 900 tcpdump -s 96 -B 10240 -i p1p1 \
   '(host mirror.umd.edu or host ftp.usf.edu or host mirrors.maine.edu or host mirrors.mit.edu or host archive.linux.duke.edu) and (port 80 or port 443)' \
